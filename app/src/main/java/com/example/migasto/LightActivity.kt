dbad8e9960d1d40d562ab9cd8bff90d7fdf8eb25
Isaac-POG <soujiok@gmail.com>
2021-08-07 19:40:39 -0400

    Second commit

--- app/src/main/java/com/example/migasto/LightActivity.kt
+++ app/src/main/java/com/example/migasto/LightActivity.kt
@@ -1,17 +1,11 @@
 package com.example.migasto
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 
-class MainActivity : AppCompatActivity() {
+class LightActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-
-    fun changeToLightMenu(view: View){
-
+        setContentView(R.layout.activity_light)
     }
 }
